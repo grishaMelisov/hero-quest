f8b38e3ccae0005a2e665347ffbc58639f2e4509
Grigoriy Melisov <urecherrydream@gmail.com>
2026-04-06 15:35:30 -0300

    [initial] init react proj + deps

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen'>
+        hello hero-quest
+      </div>
+    </>
+  );
+}
+
+export default App;
