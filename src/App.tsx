2814c7d4fc5b1b07dce4fb4a88e69b2d44bcdb45
Grigoriy Melisov <urecherrydream@gmail.com>
2026-05-19 02:21:13 -0300

    [deploy] consistency fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import AccountPage from './pages/AccountPage';
 import StubPage from './pages/StubPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Navigate to='/account' replace />} />
@@ -15,6 +15,6 @@ export default function App() {
           <Route path='*' element={<StubPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
